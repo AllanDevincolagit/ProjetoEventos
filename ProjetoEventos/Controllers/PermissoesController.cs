8b7988a99fc074688c7564587464b3aff45e3619
ALLAN DEVINCOLA <profissional.allandevincola@gmail.com>
2022-07-29 09:23:04 -0300

    permissoes controller feito

---
+++ ProjetoEventos/Controllers/PermissoesController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProjetoEventos.Entidades;
+
+namespace ProjetoEventos.Controllers
+{
+    public class PermissoesController : Controller
+    {
+        private readonly Contexto db;
+        public PermissoesController(Contexto contexto)
+        {
+            db = contexto;
+        }
+        // GET: PermissoesController
+        public ActionResult Index()
+        {
+            return View(db.PERMISSOES.ToList());
+        }
+
+        // GET: PermissoesController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View(db.PERMISSOES.Where(a=>a.ID == id).FirstOrDefault());
+        }
+
+        // GET: PermissoesController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PermissoesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Permissoes collection)
+        {
+            try
+            {
+                db.PERMISSOES.Add(collection);
+                db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PermissoesController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View(db.PERMISSOES.Where(a=>a.ID == id).FirstOrDefault());
+        }
+
+        // POST: PermissoesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Permissoes collection)
+        {
+            try { 
+            
+                db.PERMISSOES.Update(collection);
+                db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PermissoesController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View(db.PERMISSOES.Where(a=>a.ID == id).FirstOrDefault());
+        }
+
+        // POST: PermissoesController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Permissoes collection)
+        {
+            try
+            {
+                db.PERMISSOES.Remove(db.PERMISSOES.Where(a => a.ID == id).FirstOrDefault());
+                db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
